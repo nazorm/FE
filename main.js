c6f2ca6049d4cd3393a1827b30f8c0f6a38d5096
Nazorm <nnadoziechinazor@gmail.com>
2020-09-08 20:37:03 +0100

    UI Conversion of different pages

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let landing = document.querySelector(".landing")
 let logIn = document.querySelector(".logInPage")
 let signup = document.querySelector(".signupPage")
 let dashboard = document.querySelector(".dashboard")
+let resourcePage = document.querySelector(".resource-section")
+let eventPage = document.querySelector(".event-section")
 
 const displayDropdown =() =>{
     dropdown.style.display = "block"
@@ -39,4 +41,33 @@ const goToDashboard = ()=>{
     landing.style.display = "none"
     signup.style.display = "none"
     logIn.style.display = "none"
+}
+
+const dashboardArrow = (index)=>{
+if (index === 0){
+  let academy = document.querySelector(".academy");
+  academy.style.display = "none"
+
+}else if(index === 3){
+    dashboard.style.display = "none"
+    dropdown.style.display = "none" 
+    landing.style.display = "none"
+    signup.style.display = "none"
+    logIn.style.display = "none"
+    resourcePage.style.display = "block"
+}else if (index === 7){
+    eventPage.style.display = "block"
+    dashboard.style.display = "none"
+    dropdown.style.display = "none" 
+    landing.style.display = "none"
+    signup.style.display = "none"
+    logIn.style.display = "none"
+    resourcePage.style.display = "none"
+}
+
+
+
+
+
+
 }
